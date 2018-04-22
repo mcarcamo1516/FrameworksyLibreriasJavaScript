78d64b8379b9f7e2bd7def4aaf109515197b8d65
mcarcamo1516 <35343238+mcarcamo1516@users.noreply.github.com>
2018-04-22 18:16:20 -0300

    mostrar tablero

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 var tablero = new Array();
 var i = 0;
 var ii = 0;
+var cont1 = 1;
+//escuachas
+$(".btn-reinicio").click( function(){
+    llenartablero();
+    mostrartablero();
+});
 
+
+//funciones
 function llenartablero(){
     tablero = [];
+    i=0;
     while(i < 7){
         tablero[i]=new Array();
         ii=0;
@@ -12,6 +21,21 @@ function llenartablero(){
             ii ++;
         }
         i++;
+    }
+    
+}
+function mostrartablero(){
+    i=0;
+    ii=0;
+    cont1=0;
+    while(ii < 7){
+        i=0;
+        while(i < 7){
+            $(".col-"+cont1).prepend("<img src='image/"+tablero[i][ii]+".png' class='elemento'>");
+            i++;
+        }
+        ii++;
+        cont1++;
     }
     
 }
