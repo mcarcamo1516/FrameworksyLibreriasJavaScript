b247364160f6ae0b7b3184520f013e62c0936a27
mcarcamo1516 <35343238+mcarcamo1516@users.noreply.github.com>
2018-04-22 10:55:35 -0300

    llenar tablero

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
+var tablero = new Array();
+var i = 0;
+var ii = 0;
 
-Math.floor(Math.random() * 4) + 1;
+function llenartablero(){
+    tablero = [];
+    while(i < 7){
+        tablero[i]=new Array();
+        ii=0;
+        while(ii < 7){
+            tablero[i][ii] = Math.floor(Math.random() * 4) + 1;
+            ii ++;
+        }
+        i++;
+    }
+    
+}
