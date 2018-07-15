d0d6628331e5e05c0c7c2bcb871c88c631bc0163
mcarcamo1516 <35343238+mcarcamo1516@users.noreply.github.com>
2018-07-15 02:13:49 -0400

    validacion filas
    validacion filas

--- js/app.js
+++ js/app.js
@@ -47,3 +47,62 @@ function llenartablero() {
 	agregardulceevento();
 	validacion();
 }
+
+
+function validacion() {
+	validacioncolumnas();
+	validacionfilas();
+	if ($('img.delete').length !== 0) {
+		eliminaranimacion();
+	}
+}
+
+function validacionfilas() {
+	for (var j = 0; j < 6; j++) {
+		var contador = 0;
+		var candyPosition = [];
+		var extraCandyPosition = [];
+		var candyRow = candyRows(j);
+		var comparisonValue = candyRow[0];
+		var validador = false;
+		for (var i = 1; i < candyRow.length; i++) {
+			var srcComparison = comparisonValue.attr('src');
+			var srcCandy = candyRow[i].attr('src');
+
+			if (srcComparison != srcCandy) {
+				if (candyPosition.length >= 3) {
+					validador = true;
+				} else {
+					candyPosition = [];
+				}
+				contador = 0;
+			} else {
+				if (contador == 0) {
+					if (!validador) {
+						candyPosition.push(i - 1);
+					} else {
+						extraCandyPosition.push(i - 1);
+					}
+				}
+				if (!validador) {
+					candyPosition.push(i);
+				} else {
+					extraCandyPosition.push(i);
+				}
+				contador += 1;
+			}
+			comparisonValue = candyRow[i];
+		}
+		if (extraCandyPosition.length > 2) {
+			candyPosition = $.merge(candyPosition, extraCandyPosition);
+		}
+		if (candyPosition.length <= 2) {
+			candyPosition = [];
+		}
+		candyCount = candyPosition.length;
+		if (candyCount >= 3) {
+			deleteHorizontal(candyPosition, candyRow);
+			setScore(candyCount);
+		}
+	}
+}
